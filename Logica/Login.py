29a3f364ee30fdd45e1a5b5f4757137a4b3ca172
Luih <l.seba.aburto@gmail.com>
2021-12-02 22:37:35 -0300

    Modificación pantalla de acceso
    Se le agregó el estilo a la pantalla

--- Logica/Login.py
+++ Logica/Login.py
@@ -14,28 +14,28 @@ def Login():
     origen = str(pathlib.Path().resolve())
     path = origen[:len(origen)-6]
     
-    imagen = Image.open(path+"Recursos\\Iconos\\fondo_login.png")
+    imagen = Image.open("MenuAcceso.png")
     render = ImageTk.PhotoImage(imagen)
     
     fondo1 = ttk.Label(window, image=render)
     fondo1.image = render
     fondo1.pack()
-    fondo1.place(x=-1, y=-1)
+    fondo1.place(x=-5, y=-2)
     
     entrada1 = ttk.Entry(window, textvariable=user)
-    entrada1.place(width=240, height=30, x=280, y=305)
+    entrada1.place(width=240, height=30, x=315, y=255)
     
     entrada2 = ttk.Entry(window, textvariable=password)
-    entrada2.place(width=240, height=30, x=280, y=365)
+    entrada2.place(width=240, height=30, x=315, y=335)
     
-    boton1 = ttk.Button(text="Iniciar Sesión", command=Verificar)
-    boton1.place(width=170, height=50, x=315, y=425)
+    boton1 = ttk.Button(text="ACCEDER", command=Verificar)
+    boton1.place(width=170, height=50, x=345, y=425)
 
 if __name__ == '__main__':
     
     window = tkinter.Tk()
     window.title('Facial Recognition')
-    window.geometry("800x600")
+    window.geometry("829x600")
     window.resizable(0,0)
     
     user = tkinter.StringVar()
